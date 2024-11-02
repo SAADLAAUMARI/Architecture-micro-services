cd20200d0beb494f4d8bb06a9b353978afe066c5
SAAD <laaumaris@gmail.com>
2024-11-02 23:13:42 +0100

    second commit

--- gateway/src/main/java/org/sid/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/sid/gateway/GatewayApplication.java
@@ -19,7 +19,7 @@ public class GatewayApplication {
 	RouteLocator routeLocator(RouteLocatorBuilder builder){
 	return builder.routes()
 			.route("r1", r -> r.path("/customers/**").uri("lb://CUSTOMER-SERVICE"))
-			.route("r2", r -> r.path("/products/**").uri("lb://INVENTORY-SERVICE"))
+			.route("r2", r -> r.path("/products/**").uri("lb://PRODUCT-SERVICE"))
 			.build();
 	}
 
